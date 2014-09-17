zyphlar/influxdb

commit 25cc9793c28ca44d84f25bb1a93134ad1ded62c2

Use double quotes in GetQueryString()
Fix #923

parser/from_clause.go
@@ -57,7 +57,7 @@ func (self *FromClause) GetString() string {
 			if t.Alias != "" {
 				alias = fmt.Sprintf(" as %s", t.Alias)
 			}
-			names = append(names, fmt.Sprintf("%s%s", t.Name.GetString(), alias))
+			names = append(names, fmt.Sprintf(`"%s"%s`, t.Name.GetString(), alias))
 		}
 		buffer.WriteString(strings.Join(names, ","))
 	}

parser/parser_test.go
@@ -73,6 +73,20 @@ func (self *QueryParserSuite) TestParseBasicSelectQuery(c *C) {
 	}
 }
 
+func (self *QueryParserSuite) TestGetQueryStringWithDoubleQuotes(c *C) {
+	q := `select dashboard from "grafana.dashboard_VWx0cmFNUg=="`
+	query, err := ParseQuery(q)
+	c.Assert(err, IsNil)
+	c.Assert(query, HasLen, 1)
+	actualQ := query[0].GetQueryStringWithTimeCondition()
+	actualQuery, err := ParseQuery(actualQ)
+	c.Assert(err, IsNil)
+	c.Assert(actualQuery, HasLen, 1)
+	query[0].SelectQuery.startTimeSpecified = false
+	actualQuery[0].SelectQuery.startTimeSpecified = false
+	c.Assert(actualQuery, DeepEquals, query)
+}
+
 func (self *QueryParserSuite) TestGetQueryString(c *C) {
 	for _, query := range []string{
 		"select value from t",

commit e43b920f3b2ce70d797d496abdd5abd34c64a89b

Fix a bug introduced in 25cc9793c28ca44d84f25bb1a93134ad1ded62c2

parser/from_clause.go
@@ -57,6 +57,10 @@ func (self *FromClause) GetString() string {
 			if t.Alias != "" {
 				alias = fmt.Sprintf(" as %s", t.Alias)
 			}
+			if t.Name.Type == ValueRegex {
+				names = append(names, fmt.Sprintf(`%s%s`, t.Name.GetString(), alias))
+				continue
+			}
 			names = append(names, fmt.Sprintf(`"%s"%s`, t.Name.GetString(), alias))
 		}
 		buffer.WriteString(strings.Join(names, ","))