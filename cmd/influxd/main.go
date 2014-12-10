8f1d068d7d23407c8a312695cdebe2c0adbbe208
Ben Johnson <benbjohnson@yahoo.com>
2014-12-10 15:20:13 -0700

    Refactor CLI commands.

--- cmd/influxd/main.go
+++ cmd/influxd/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"flag"
+	"encoding/json"
 	"fmt"
+	"log"
 	"os"
+	"strings"
 )
 
 const logo = `
@@ -19,19 +21,60 @@ const logo = `
 
 // These variables are populated via the Go linker.
 var (
-	version string
+	version string = "0.9"
 	commit  string
 )
 
 func main() {
-	if err := start(); err != nil {
-		fmt.Println(err.Error())
-		os.Exit(1)
+	log.SetFlags(0)
+
+	// Shift binary name off argument list.
+	args := os.Args[1:]
+
+	// Retrieve command name as first argument.
+	var cmd string
+	if len(args) > 0 && !strings.HasPrefix(args[0], "-") {
+		cmd = args[0]
 	}
-	os.Exit(0)
+
+	// If command is "help" and has an argument then rewrite args to use "-h".
+	if cmd == "help" && len(args) > 1 {
+		args[0], args[1] = args[1], "-h"
+		cmd = args[0]
+	}
+
+	// Extract name from args.
+	switch cmd {
+	case "create-cluster":
+		execCreateCluster(args[1:])
+	case "join-cluster":
+		execJoinCluster(args[1:])
+	case "run":
+		execRun(args[1:])
+	case "":
+		execRun(args)
+	case "version":
+		execVersion(args[1:])
+	case "help":
+		execHelp(args[1:])
+	default:
+		log.Fatalf(`influxd: unknown command "%s"`+"\n"+`Run 'influxd help' for usage`+"\n\n", cmd)
+	}
 }
 
-var usageMessageHeader = `
+// execVersion runs the "version" command.
+// Prints the commit SHA1 if set by the build process.
+func execVersion(args []string) {
+	s := fmt.Sprintf("InfluxDB v%s", version)
+	if commit != "" {
+		s += fmt.Sprintf(" (git: %s)", commit)
+	}
+	log.Print(s)
+}
+
+// execHelp runs the "help" command.
+func execHelp(args []string) {
+	fmt.Println(`
 Configure and start an InfluxDB server.
 
 Usage:
@@ -40,64 +83,43 @@ Usage:
 
 The commands are:
 
-`
+    create-cluster       create a new node that other nodes can join to form a new cluster
+    join-cluster         create a new node that will join an existing cluster
+    run                  run node with existing configuration
+    version              displays the InfluxDB version
 
-func usage() {
-	fmt.Fprintf(os.Stderr, usageMessageHeader)
-	for _, c := range commands {
-		fmt.Fprintf(os.Stderr, "    %-20s %-10s\n", c.Name, c.Terse)
-	}
-	fmt.Fprintf(os.Stderr, "\n\"run\" is the default command.\n")
-	fmt.Fprintf(os.Stderr, "\nUse \"influxd help [command]\" for more information about a command.\n\n")
-	os.Exit(2)
-}
+"run" is the default command.
 
-func help(args []string) {
-	if len(args) == 0 {
-		usage()
-		return // Succeeded already at 'influxd help'
-	}
-	if len(args) != 1 {
-		fmt.Fprintf(os.Stderr, "usage: influxd help command\n\nToo many arguments given.\n")
-		os.Exit(2)
-	}
-
-	for _, cmd := range commands {
-		if cmd.Name == args[0] {
-			fmt.Fprintf(os.Stderr, "usage: %s %s\n", cmd.Name, cmd.Options)
-			fmt.Fprintf(os.Stderr, "%s\n", cmd.Long)
-			return // succeeded at 'influxd help command'
-		}
-	}
-
-}
-
-func start() error {
-	flag.Usage = usage
-	flag.Parse()
-
-	args := flag.Args()
-	if len(args) < 1 {
-		args = append(args, "run")
-	}
-
-	if args[0] == "help" {
-		help(args[1:])
-		return nil
-	}
-
-	for _, cmd := range commands {
-		if cmd.Name == args[0] {
-			cmd.Flag.Usage = func() { cmd.Usage() }
-			cmd.Flag.Parse(args[1:])
-			args = cmd.Flag.Args()
-			return cmd.Exec(cmd, args)
-		}
-	}
-
-	return fmt.Errorf("influxd: unknown command %q\nRun 'influxd help' for usage", args[0])
+Use "influxd help [command]" for more information about a command.
+`)
 }
 
 type Stopper interface {
 	Stop()
+}
+
+type State struct {
+	Mode string `json:"mode"`
+}
+
+// createStateIfNotExists returns the cluster state, from the file at path.
+// If no file exists at path, the default state is created, and written to the path.
+func createStateIfNotExists(path string) (*State, error) {
+	// Read state from path.
+	// If state doesn't exist then return a "local" state.
+	f, err := os.Open(path)
+	if os.IsNotExist(err) {
+		return &State{Mode: "local"}, nil
+	} else if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	// Decode state from file and return.
+	s := &State{}
+	if err := json.NewDecoder(f).Decode(&s); err != nil {
+		return nil, err
+	}
+
+	return s, nil
 }
